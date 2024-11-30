52e021f0cbf9f34d6442d44107e0bd5990afa289
mersal.developing@gmail.com <mersal.developing@gmail.com>
2024-11-30 14:01:07 +0400

    connect to mongodb

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
-const port = 3000;
+
+const port = process.env.PORT;
+
+mongoose
+  .connect(process.env.MONGO_DB)
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(port, () => {
   console.log(`server is running on ${port}`);
