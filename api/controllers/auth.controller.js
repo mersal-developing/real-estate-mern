2a3e68a087284bda211fa24a4ff1c6c1435de2f0
mersal.developing@gmail.com <mersal.developing@gmail.com>
2024-12-01 14:58:11 +0400

    finish sign in api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bycryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signUp = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -22,6 +24,31 @@ export const signUp = async (req, res, next) => {
     res
       .status(201)
       .json({ message: "User created successfully", user: newUser });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) return next(errorHandler(404, "User not found!"));
+
+    const validPassword = bycryptjs.compareSync(
+      password,
+      existingUser.password
+    );
+
+    if (!validPassword) return next(errorHandler(401, "wrong credentials!"));
+    const token = jwt.sign({ id: existingUser._id }, process.env.JWT_SECRET);
+
+    const { password: pass, ...resUserData } = existingUser._doc;
+
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(resUserData);
   } catch (error) {
     next(error);
   }
